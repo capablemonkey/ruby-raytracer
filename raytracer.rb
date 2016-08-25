b4694160b0d870bdc019fe7c07fc1ab897ca87e4
Gordon Zheng <gordon@greenhouse.io>
2016-08-24 20:08:18 -0400

    Add remaining shapes from example

--- raytracer.rb
+++ raytracer.rb
@@ -268,8 +268,8 @@ class Ray
 end
 
 def render(spheres)
-  width = 255
-  height = 255
+  width = 640
+  height = 480
   fov = 30
   aspect_ratio = width.to_f / height
   angle = Math.tan(Math::PI * 0.5 * fov / 180)
@@ -296,7 +296,7 @@ end
 
 def main
   bg = Sphere.new(
-    :center => Vec3.new(0, -10002, -20),
+    :center => Vec3.new(0, -10004, -20),
     :radius => 10000,
     :surface_color => Vec3.new(0.2, 0.2, 0.2),
     :reflection => 0,
@@ -312,23 +312,39 @@ def main
   )
 
   b = Sphere.new(
-    :center => Vec3.new(4.0, -1.0, -15),
+    :center => Vec3.new(5.0, -1.0, -15),
     :radius => 2,
     :surface_color => Vec3.new(0.90, 0.76, 0.46),
     :reflection => 1,
     :transparency => 0
   )
 
+  c = Sphere.new(
+    :center => Vec3.new(5.0, 0, -25),
+    :radius => 3,
+    :surface_color => Vec3.new(0.65, 0.77, 0.97),
+    :reflection => 1,
+    :transparency => 0
+  )
+
+  d = Sphere.new(
+    :center => Vec3.new(-5.5, 0, -15),
+    :radius => 3,
+    :surface_color => Vec3.new(0.9, 0.9, 0.9),
+    :reflection => 1,
+    :transparency => 0
+  )
+
   light = Sphere.new(
     :center => Vec3.new(0, 20, -30),
-    :radius => 20,
+    :radius => 3,
     :surface_color => Vec3.new(0, 0, 0),
     :reflection => 0,
     :transparency => 0,
     :emission_color => Vec3.new(3, 3, 3)
   )
 
-  render([a, b, light])
+  render([bg, a, b, c, d, light])
 end
 
 main
